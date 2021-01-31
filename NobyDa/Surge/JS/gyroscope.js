e94de89c980c5d442017039b8a8b459add27b03d
Killian.Q <41661487+KillianQi@users.noreply.github.com>
2021-02-01 00:27:54 +0800

    Update gyroscope.js

--- NobyDa/Surge/JS/gyroscope.js
+++ NobyDa/Surge/JS/gyroscope.js
@@ -4,7 +4,7 @@ Gyroscope unlock pro (Script author: @Maasea )
 Surge:
 http-response ^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/gyroscope.js
 QuantumultX:
-^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/gyroscope.js
+^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ url script-response-body https://raw.githubusercontent.com/KillianQi/QuantumultX/master/NobyDa/Surge/JS/gyroscope.js
 QX & Surge MITM = api.gyrosco.pe
 */
 
